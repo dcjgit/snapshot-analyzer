b9c0267176cd76106c4b97be687a2e078d61affa
dcjgit <rithikaren@gmail.com>
2018-05-29 10:58:11 -0400

    Updated the script to use the click module, and show more information about instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,11 +4,22 @@ os.environ['HTTP_PROXY']="http://nibr-proxy.global.nibr.novartis.net:2011"
 os.environ['HTTPS_PROXY']="http://nibr-proxy.global.nibr.novartis.net:2011"
 
 import boto3
+import click
+session = boto3.Session(profile_name='personal')
+ec2 = session.resource('ec2')
+
+#The @ is a decorator (wrapper) for our functions
+@click.command()
+def list_instances():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+        print(', '.join( (i.id,
+                          i.instance_type,
+                          i.placement['AvailabilityZone'],
+                          i.state['Name'],
+                          i.public_dns_name))
+             )
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='personal')
-    ec2 = session.resource('ec2')
-
-    print('The following are the instances in my Personal acccount dev-dj')
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
