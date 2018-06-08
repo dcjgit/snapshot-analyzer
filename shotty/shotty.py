b151825b9846cd6ee88ad212cff650dd19f021b2
dcjgit <rithikaren@gmail.com>
2018-06-08 19:40:19 -0400

    Update the code for 'list' command line argument

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -9,7 +9,10 @@ session = boto3.Session(profile_name='personal')
 ec2 = session.resource('ec2')
 
 #The @ is a decorator (wrapper) for our functions
-@click.command()
+@click.group()
+def instances():
+    """Commands for instances"""
+@instances.command('list')
 @click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
 def list_instances(project):
     "List EC2 instances"
@@ -35,5 +38,6 @@ def list_instances(project):
     return
 
 if __name__ == '__main__':
-    list_instances()
-
+    #list_instances()
+    instances()
+    
